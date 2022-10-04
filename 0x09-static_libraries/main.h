2609167e6081df227ae44752090e993570bb990b
thungusheila <thungusheila@gmail.com>
2022-10-04 03:09:22 -0700

    static libraries

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,3 +1,11 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+
+
+
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +45,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+
+
+#endif /* MAIN_H */
