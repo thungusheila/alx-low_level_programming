4a3a99563ca4e3e16e0caf0a2b6b9922700f5d4b
thungusheila <thungusheila@gmail.com>
2022-10-24 07:55:45 -0700

    singly linkrd lists

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,41 +1,38 @@
 #include "lists.h"
+#include <string.h>
 
 /**
- * _strlen - counts the lenght of a string
- * @str: string to be counted
- * Return: len of a string
- */
-
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
-/**
- * add_node - adds a node at the beginning
- * @head: first parameter
- * @str: second parameter
- * Return: nodes
+ * add_node - Adds a new node at the beginning
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new element.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
+	char *dup;
+	int len;
 	list_t *new;
 
 	new = malloc(sizeof(list_t));
-
 	if (new == NULL)
+		return (NULL);
+
+	dup = strdup(str);
+	if (dup == NULL)
 	{
+		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
-	new->len = _strlen(str);
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = dup;
+	new->len = len;
 	new->next = *head;
 
 	*head = new;
